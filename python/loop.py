5a2edf589600ba5531e07721cb4007ad7ee7bf77
aparnaapss <32951413+aparnaapss@users.noreply.github.com>
2017-10-21 13:25:01 +0530

    Add files via upload

---
+++ python/loop.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece002
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece002 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+count = 0
+while (count < 6):
+   print 'hello:', count
+   count = count + 1
+print "Good bye!"
