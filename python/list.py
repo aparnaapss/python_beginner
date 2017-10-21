dc34fc0dfa4f550465095d9d3c5a56275519cbde
aparnaapss <32951413+aparnaapss@users.noreply.github.com>
2017-10-21 14:24:50 +0530

    Add files via upload

---
+++ python/list.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece002
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece002 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+my_list = ['ranju', 'subu', 'bagss', 'myth', 'nisanth','345']
+my_list[4]="guvi"
+print (my_list[4])
+
+my_list = ('ranju', 'subu', 'bagss', 'myth', 'nisanth', '345')
+'''my_list[4]="guvi"'''
+print (my_list[4])
