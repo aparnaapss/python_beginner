b053b809f7c144ad9d57d1a1450f248be927f096
aparnaapss <32951413+aparnaapss@users.noreply.github.com>
2017-10-21 10:58:18 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece002
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece002 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+mystring = 'hi fromguvi' + 'test'
+print (mystring[3:6])
+print (mystring[2:4])
+print (mystring.split(" "))
+print (mystring.find("u"))
