98a704c5a6b8cbdd917105298bb62da0f18a3223
aparnaapss <32951413+aparnaapss@users.noreply.github.com>
2017-10-21 11:13:04 +0530

    Add files via upload

---
+++ branching.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16ece002
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16ece002 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=10
+if (a<18):
+     print ("a is less then 18")
+elif (a>18 and a<10):
+       print ("a is in between")
+else:
+    print ("a is mental")
